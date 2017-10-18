497fef583a70a3c872410dd59439dba8f5ffd2fb
A-nas <anass.laghouaouta@hotmail.fr>
2017-10-18 06:31:47 +0200

    add new request header to infinite loop url

--- makeScrapingGreatAgain/spiders/trumpSpider.py
+++ makeScrapingGreatAgain/spiders/trumpSpider.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
-from scrapy.http import Request # for yelding a request 
+from scrapy import FormRequest
+from scrapy.http import Request # for yelding a request
+import logging
 
 
 class TrumpspiderSpider(Spider):
@@ -11,19 +13,29 @@ class TrumpspiderSpider(Spider):
     def __init__(self, profileName, since, until): # date should be formated as follow yyyy-mm-dd
     	self.start_urls = ['https://twitter.com/search?l=&q=from:'+profileName+
                            ' since:'+ since +
-                           ' until:'+ until +'&src=typd']	   	
+                           ' until:'+ until +'&src=typd']
+
+    #URL headers
+    def start_requests(self):
+        params = { 
+        'Host':'twitter.com',
+        'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/56.0',
+        'Accept':'text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8',
+        'Accept-Language':'en-US,en;q=0.5',
+        'Accept-Encoding':'gzip, deflate, br',
+        'Upgrade-Insecure-Requests':'1',
+        'Connection':'keep-alive',
+        'Cache-Control':'max-age=0'
+        }
+        return [FormRequest(self.start_urls[0],headers=params)]#default callback will be parse function   	
 
     def parse(self, response):
-        # must scrap tweet date
         comments_react = self.stats_extractor('reply', response)
         retweet_react = self.stats_extractor('retweet', response)
         favorite_react = self.stats_extractor('favorite', response)
-
         tweets = response.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[2]/p/text()').extract()
         tweetdates = response.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[1]/small/a/span[1]/text()').extract();
-        
-        #search for next url and run callback
-        
+        #maxPosition = response.xpath();
         yield {
          'comments' : comments_react,
          'retweets' : retweet_react,
@@ -31,6 +43,13 @@ class TrumpspiderSpider(Spider):
          'teweets' : tweets,
          'tweetDates' : tweetdates,
         }
+        #search for next position
+        params = { 
+        "action": "search",
+        "description": "My search here",
+        "e_author": ""
+        }
+        yield Request(response.url, callback=self.parse_tweets)
 
     def stats_extractor(self, statsString, response):
         #make an enum for that (parameter)
