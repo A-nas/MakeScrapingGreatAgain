71857ad78fd291cac98a397382bbbe6a73b9771e
A-nas <anass.laghouaouta@hotmail.fr>
2017-10-20 21:36:41 +0200

    bug max-position solved

--- makeScrapingGreatAgain/spiders/trumpSpider.py
+++ makeScrapingGreatAgain/spiders/trumpSpider.py
@@ -18,8 +18,23 @@ class TrumpspiderSpider(Spider):
     since = None
     until = None
     query = None
+    last_position = None
     #computd properties
     start_urlsGetParams = None
+
+        #overrided fucntion that prepare request with inner headers
+#    def start_requests(self):
+#        params = { 
+#        'Host':'twitter.com',
+#        'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/56.0',
+#        'Accept':'text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8',
+#        'Accept-Language':'en-US,en;q=0.5',
+#        'Accept-Encoding':'gzip, deflate, br',
+#        'Upgrade-Insecure-Requests':'1',
+#        'Connection':'keep-alive',
+#        'Cache-Control':'max-age=0'
+#        }
+#        return [FormRequest(self.start_urls[0],headers=params)]#default callback will be parse function  
 
     def __init__(self, profileName, since, until):
         self.profileName = profileName
@@ -34,11 +49,10 @@ class TrumpspiderSpider(Spider):
         self.allowed_domains = ['https://twitter.com/'+self.profileName]
         self.tweetAPIGetParams = ['https://twitter.com/i/profiles/show/'+self.profileName+'/timeline/tweets']
 
-    	self.start_urls = ['https://twitter.com/search?'+urlencode(self.start_urlsGetParams)]
-        logging.info('warning call %s',self.start_urls[0])
+    	self.start_urls = ['https://twitter.com/search?'+urlencode(self.start_urlsGetParams)] 	
 
-    #overrided fucntion that prepare request with inner headers
-    def start_requests(self):
+    def parse(self, response):
+        #self.parse_data(response)
         params = { 
         'Host':'twitter.com',
         'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/56.0',
@@ -49,39 +63,53 @@ class TrumpspiderSpider(Spider):
         'Connection':'keep-alive',
         'Cache-Control':'max-age=0'
         }
-        return [FormRequest(self.start_urls[0],headers=params)]#default callback will be parse function   	
-
-    def parse(self, response):
-        self.parse_data(response)
+        yield Request(response.url, callback=self.parse_data,dont_filter=True, headers=params)
         #search for next iteration (min-position)
-        next_position = response.xpath('//*[@id="timeline"]/div/@data-min-position').extract()[0].split('-')[1]
-        logging.info('Next Position ====> %s',next_position)
+        next_position = response.xpath('//*[@id="timeline"]/div/@data-min-position').extract()[1].split('-')[1]
+        last_position = response.xpath('//*[@id="timeline"]/div/@data-min-position').extract()[1].split('-')[2]
         #GET parameters (no headers needed (for the moment))
         get_params = {
         'q' : self.query,
         'include_available_features' : '1',
         'include_entities' : '1',
-        'max_position' : next_position,
+        'max_position' : 'TWEET-'+next_position+'-'+last_position,
         'reset_error_state' : 'false',
         'src':'typd',
         'vertical':'default'
         }
+
         sleep(10)
+        logging.info('============================= IM HERE 1============================================= ')
         yield Request(self.tweetAPIGetParams[0]+'?'+urlencode(get_params), callback=self.parse_json_tweets,dont_filter=True)
 
 
     def parse_json_tweets(self, response):
+        logging.info('============================= IM HERE 2============================================= ')
         data = json.loads(response.text)
         selector = Selector(text=data['items_html'], type='html')
-        self.parse_data(reponse = selector)
+        #self.parse_data(selector) dont work
+        comments_react = self.stats_extractor('reply', selector)
+        retweet_react = self.stats_extractor('retweet', selector)
+        favorite_react = self.stats_extractor('favorite', selector)
+        tweets = selector.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[2]/p/text()').extract()
+        tweetdates = selector.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[1]/small/a/span[1]/text()').extract();
+        yield {
+         'comments' : comments_react,
+         'retweets' : retweet_react,
+         'favorites' : favorite_react,
+         'teweets' : tweets,
+         'tweetDates' : tweetdates,
+        }
+        #yield Request(response.url, callback=self.parse_data,dont_filter=True)
 
     def parse_data(self, response):
-        logging.info(' EXTRACTIONNN ====> %s',response.url)
+        #logging.info(' EXTRACTIONNN ====> %s',response.url)
         comments_react = self.stats_extractor('reply', response)
         retweet_react = self.stats_extractor('retweet', response)
         favorite_react = self.stats_extractor('favorite', response)
         tweets = response.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[2]/p/text()').extract()
         tweetdates = response.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[1]/small/a/span[1]/text()').extract();
+        logging.info('============================= yelding ============================================= ')
         yield {
          'comments' : comments_react,
          'retweets' : retweet_react,
