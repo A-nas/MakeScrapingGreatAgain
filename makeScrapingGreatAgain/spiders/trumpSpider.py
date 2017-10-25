f379c59a56beeabf614fdb39cfd57be03380874e
A-nas <anass.laghouaouta@hotmail.fr>
2017-10-25 09:50:30 +0200

    works fine, json parsed

--- makeScrapingGreatAgain/spiders/trumpSpider.py
+++ makeScrapingGreatAgain/spiders/trumpSpider.py
@@ -8,7 +8,7 @@ from urllib import urlencode
 import logging
 import json
 
-
+#selector must be exported to config files
 class TrumpspiderSpider(Spider):
     #overrided properties
     name = 'trumpSpider'
@@ -19,22 +19,62 @@ class TrumpspiderSpider(Spider):
     until = None
     query = None
     last_position = None
+    params = None
+    cookies = None
+    API_Cookies = {
+    'personalization_id' :'v1_AxNAd+cMYAuU4SmQcsKBlw==',
+    'guest_id' :'v1%3A150592161834956482',
+    '_ga' :'GA1.2.1438276811.1506337821', 
+    'eu_cn' :'1',
+    'kdt' :'Jtqd64V6rkpRKrALlMD8S0PXa4B93rSU9idtTb0E',
+    'remember_checked_on' :'1', 
+    'ads_prefs' :'HBERAAA=',
+    'twid' :'u=3462555082',
+    'auth_token' :'f415a8976fe683be29fbbef3bbce3855b92a8c16',
+    'tip_nightmode' :'true',
+    'tfw_exp' :'0',
+    '__utma' :'43838368.1438276811.1506337821.1508530210.1508547526.5',
+    '__utmz' :'43838368.1508320207.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)',
+    '_gid' :'GA1.2.820665622.1508520108',
+    '_twitter_sess' :'BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCLho0EBfAToMY3NyZl9p%250AZCIlOGYxNjQ3OTUxNzUyMjc3ZmM2ZTI2M2YyZGFjMjdlODA6B2lkIiVjYzcw%250AMDIyOTczYmQ3OWRkNjU3MzNlZTJlNTZlOGZjZQ%253D%253D--9dffee641f695f6e52d049fd90d1ebf823f57095',
+    'lang' :'fr',
+    'ct0' :'5eec94032ed6d21e015d80900952cb5d'
+    }
+
     #computd properties
     start_urlsGetParams = None
-
         #overrided fucntion that prepare request with inner headers
-#    def start_requests(self):
-#        params = { 
-#        'Host':'twitter.com',
-#        'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/56.0',
-#        'Accept':'text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8',
-#        'Accept-Language':'en-US,en;q=0.5',
-#        'Accept-Encoding':'gzip, deflate, br',
-#        'Upgrade-Insecure-Requests':'1',
-#        'Connection':'keep-alive',
-#        'Cache-Control':'max-age=0'
-#        }
-#        return [FormRequest(self.start_urls[0],headers=params)]#default callback will be parse function  
+    def start_requests(self):
+        self.params = { 
+        'Host':'twitter.com',
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language':'en-US,en;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'Upgrade-Insecure-Requests':'1',
+        'Connection':'keep-alive',
+        'Cache-Control':'max-age=0',
+        'authority' : 'twitter.com',
+        }
+        #careful with session auth, may be expired
+        self.cookies = {
+        'personalization_id':'v1_YHTV7EBksk3kG/Ywvj8gTw==',
+        'guest_id':'v1%3A150773123259177115',
+        'eu_cn':'1',
+        'tfw_exp':'0',
+        'lang':'fr',
+        'ct0':'f0055d52b075c1ecab6a2cb01ee32357',
+        'gt':'921462777285357568',
+        '_ga':'GA1.2.2111084724.1507731254',
+        '_gid':'GA1.2.2106045753.1508528820',
+        '_twitter_sess':'BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCLIDUztfAToMY3NyZl9p%250AZCIlNWQ1ZDBjMDZlN2ViOGY5YzMzOGMxYzlhOThjYTg3MzI6B2lkIiU3ZjI5%250AYjNlOTVkYjJlMWUzOWQ3Y2ZlNzJhNjU2MDU5Nw%253D%253D--7ce3c5c371e0c5d6a5403a6d7292fd1700951f82',
+        '__utma':'43838368.2111084724.1507731254.1508532429.1508532429.1',
+        '__utmt':'1',
+        '__utmb':'43838368.1.10.1508532429',
+        '__utmc':'43838368',
+        '__utmz':'43838368.1508532429.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)'
+        }
+        return [Request(self.start_urls[0],headers=self.params,cookies=self.cookies)]#default callback will be parse function  
 
     def __init__(self, profileName, since, until):
         self.profileName = profileName
@@ -47,26 +87,15 @@ class TrumpspiderSpider(Spider):
         }
         #overrided
         self.allowed_domains = ['https://twitter.com/'+self.profileName]
-        self.tweetAPIGetParams = ['https://twitter.com/i/profiles/show/'+self.profileName+'/timeline/tweets']
-
+        #self.tweetAPIGetParams = ['https://twitter.com/i/profiles/show/'+self.profileName+'/timeline/tweets'] OLD
+        self.tweetAPIGetParams = ['https://twitter.com/i/search/timeline']
     	self.start_urls = ['https://twitter.com/search?'+urlencode(self.start_urlsGetParams)] 	
 
     def parse(self, response):
-        #self.parse_data(response)
-        params = { 
-        'Host':'twitter.com',
-        'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linu…) Gecko/20100101 Firefox/56.0',
-        'Accept':'text/html,application/xhtml+xm…plication/xml;q=0.9,*/*;q=0.8',
-        'Accept-Language':'en-US,en;q=0.5',
-        'Accept-Encoding':'gzip, deflate, br',
-        'Upgrade-Insecure-Requests':'1',
-        'Connection':'keep-alive',
-        'Cache-Control':'max-age=0'
-        }
-        yield Request(response.url, callback=self.parse_data,dont_filter=True, headers=params)
+        yield Request(response.url, callback=self.parse_data,dont_filter=True, headers=self.params)
         #search for next iteration (min-position)
-        next_position = response.xpath('//*[@id="timeline"]/div/@data-min-position').extract()[1].split('-')[1]
-        last_position = response.xpath('//*[@id="timeline"]/div/@data-min-position').extract()[1].split('-')[2]
+        next_position = response.xpath('//*[@id="timeline"]/div/@data-max-position').extract_first().split('-')[1]
+        last_position = response.xpath('//*[@id="timeline"]/div/@data-max-position').extract_first().split('-')[2]
         #GET parameters (no headers needed (for the moment))
         get_params = {
         'q' : self.query,
@@ -74,25 +103,25 @@ class TrumpspiderSpider(Spider):
         'include_entities' : '1',
         'max_position' : 'TWEET-'+next_position+'-'+last_position,
         'reset_error_state' : 'false',
-        'src':'typd',
         'vertical':'default'
         }
 
         sleep(10)
         logging.info('============================= IM HERE 1============================================= ')
-        yield Request(self.tweetAPIGetParams[0]+'?'+urlencode(get_params), callback=self.parse_json_tweets,dont_filter=True)
+        yield Request(self.tweetAPIGetParams[0]+'?'+urlencode(get_params), callback=self.parse_json_tweets,dont_filter=True,cookies=self.API_Cookies,headers=self.params)
 
 
     def parse_json_tweets(self, response):
         logging.info('============================= IM HERE 2============================================= ')
         data = json.loads(response.text)
+        logging.info('=========================+++> %s',data['new_latent_count'])
         selector = Selector(text=data['items_html'], type='html')
         #self.parse_data(selector) dont work
         comments_react = self.stats_extractor('reply', selector)
         retweet_react = self.stats_extractor('retweet', selector)
         favorite_react = self.stats_extractor('favorite', selector)
         tweets = selector.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[2]/p/text()').extract()
-        tweetdates = selector.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[1]/small/a/span[1]/text()').extract();
+        tweetdates = selector.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[1]/small/a/span[1]/text()').extract()
         yield {
          'comments' : comments_react,
          'retweets' : retweet_react,
@@ -108,7 +137,7 @@ class TrumpspiderSpider(Spider):
         retweet_react = self.stats_extractor('retweet', response)
         favorite_react = self.stats_extractor('favorite', response)
         tweets = response.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[2]/p/text()').extract()
-        tweetdates = response.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[1]/small/a/span[1]/text()').extract();
+        tweetdates = response.xpath('.//*[contains(@class,"js-stream-item stream-item stream-item")]/div[1]/div[2]/div[1]/small/a/span[1]/text()').extract()
         logging.info('============================= yelding ============================================= ')
         yield {
          'comments' : comments_react,
