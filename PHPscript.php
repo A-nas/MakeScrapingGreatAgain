906829478a773027e0c483305509d4487de4d4b9
A-nas <anass.laghouaouta@hotmail.fr>
2017-12-05 23:14:13 +0100

    PHPscript added to debug PHP mongodb extention

---
+++ PHPscript.php
@@ -0,0 +1,6 @@
+<?php
+    	$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+        $query = new MongoDB\Driver\Query(array('tweetDate' => '4 nov. 2014'));
+        $cursor = $manager->executeQuery('paperman.course', $query);
+        print_r($cursor->toArray());
+?>
