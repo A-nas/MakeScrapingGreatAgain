dd45ae520ea421ed31216ca7a45e4e7293fb92fa
A-nas <anass.laghouaouta@hotmail.fr>
2017-12-06 17:48:03 +0100

    testing json backslash return

--- PHPscript.php
+++ PHPscript.php
@@ -2,5 +2,6 @@
     	$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
         $query = new MongoDB\Driver\Query(array('tweetDate' => '4 nov. 2014'));
         $cursor = $manager->executeQuery('paperman.course', $query);
-        print_r($cursor->toArray());
+        print json_encode( $cursor->toArray() );
+        //print_r($cursor->toArray());
 ?>
