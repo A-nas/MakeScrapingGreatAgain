63b2b166e207310f72f823e5f3b1fc4debc5f837
A-nas <anass.laghouaouta@hotmail.fr>
2020-11-08 01:39:11 +0100

    not working

--- makeScrapingGreatAgain/spiders/FacebookPendingRequestSpider.py
+++ makeScrapingGreatAgain/spiders/FacebookPendingRequestSpider.py
@@ -1,5 +1,6 @@
 import scrapy
-from scrapy.http import Request
+#from scrapy.http import Request
+import requests
 import logging
 from scrapy.http import FormRequest
 import json
@@ -26,7 +27,6 @@ class FacebookpendingrequestspiderSpider(scrapy.Spider):
             #form data to send via POST
         self.data = json.loads(configSection['Datas'])
 
-
         return [FormRequest(self.start_urls[0],
                             headers=self.params,
                             method='POST',
@@ -43,6 +43,7 @@ class FacebookpendingrequestspiderSpider(scrapy.Spider):
     def parse(self, response):
         keepScroll = None
         #parse json
+        print(response.text)
         apiResponse = json.loads(response.text)
         #iterate on json and yield datas (list of 10)
         for key,value in enumerate(apiResponse['data']
@@ -54,12 +55,11 @@ class FacebookpendingrequestspiderSpider(scrapy.Spider):
              'url' : value['node']['url'],
              'name' : value['node']['name']
             }
-            self.data.update({'variables':'{"input":{"cancelled_friend_requestee_id":"'+value['node']['id']+'","source":"manage_outgoing_requests","actor_id":"100000946033663","client_mutation_id":"4"},"scale":1.5}'})
-            yield FormRequest(self.start_urls[0],
-                           headers=self.params,
-                           method='POST',
-                           formdata=self.data,
-                           cookies=self.cookies)
+            # unfriendDatas = self.data
+            # unfriendDatas.update({'variables':'{"input":{"cancelled_friend_requestee_id":"'+value['node']['id']+'","source":"manage_outgoing_requests","actor_id":"100000946033663","client_mutation_id":"4"},"scale":1.5}'})
+            # # unfriendDatas.update({'__req':'20'})
+            # unfriendDatas.update({'doc_id':'3226051994092510'}) #important (type of request)
+            # requests.post(self.start_urls[0],headers=self.params,data=unfriendDatas,cookies=self.cookies)
         #get stop iterator
         keepScroll = apiResponse['data']['viewer']['outgoing_friend_requests_connection']['page_info']['has_next_page']
 
